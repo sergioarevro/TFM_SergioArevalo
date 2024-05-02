cd7695a7e6a704db602fb94ae5d82a19f95fcbc2
sergioarevro <sergio.arevaloroa@gmail.com>
2024-05-02 17:51:52 +0200

    ERC1400 | Añadidos contratos y dependencias.

--- truffle/migrations/1_deploy_contracts.js
+++ truffle/migrations/1_deploy_contracts.js
@@ -1,8 +1,9 @@
 const SimpleStorage = artifacts.require("../contracts/SimpleStorage");
-const Voting = artifacts.require("Voting")
+const ERC1400 = artifacts.require("../contracts/ERC1400/ERC1400.sol");
 
 module.exports = function(deployer, accounts) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(ERC1400);
   //const optionNames = ["Option 1", "Option 2", "Option 3"]; // Nombres de las opciones de voto
   //deployer.deploy(Voting, optionNames);
 };
